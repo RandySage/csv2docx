fa943593ebaeccf0ab3a5932c19c0c061f426b02
Randy <randy@thinhorn.com>
2013-12-26 13:36:31 -0800

    Large refactor; next add images and references

--- csv2docx.py
+++ csv2docx.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
 
 """
 This file makes a .docx (Word 2007) file from scratch, showing off most of the
@@ -14,7 +14,11 @@ See LICENSE for licensing information.
 from docx import *
 import re
 import csv
+import sys
+import json
+import inspect
 
+DEFAULT_JSON = 'settings.json'
 INPUT_FILE = 'test/input.csv'
 OUTPUT_FILE = 'test/output.docx'
 NUM_NON_BODY_COLS = 5
@@ -30,8 +34,46 @@ subject  = 'Auto export using docx from Python'
 creator  = 'Randy Sage'
 keywords = ['python', 'Office Open XML', 'Word']
 
-if __name__ == '__main__':
+
+class MySettings():
+
+    def get_json(self, json_filename):
+        try:
+            with open(json_filename) as json_file:
+                json_dict = json.loads(json_file.read())
+                return json_dict
+        except ValueError:
+            sys.exit("Parse error for json file: %s\nExiting..." % json_filename)
+        except IOError:
+            sys.exit("Exiting from %s because failed to open json file: %s\nExiting..." % 
+                     (inspect.stack()[0][3],json_filename))
+    # get_json
     
+    def read_json(self):
+        try:
+            settings = self.get_json(DEFAULT_JSON)
+            # try:
+            #     s.l_delim = settings.reference.start
+            #     s.r_delim = settings.reference.end
+            # except:
+            #     sys.exit("Exiting with issue in get_settings\nExiting...")
+            if ( len(settings['l_delim']) > 1 or
+                 len(settings['r_delim']) > 1 ):
+                sys.exit(
+                    ("l_delim ('%s') and r_delim ('%s') need to be single characters\nExiting..." % 
+                     (settings['l_delim'], settings['r_delim'])))
+            self.l_delim = settings['l_delim'] 
+            self.r_delim = settings['r_delim']
+            return s
+        except 23:
+            sys.exit("Unexpected issue in %s\nExiting..." %
+                     inspect.stack()[0][3])
+    # read_json
+
+# MySettings
+
+
+class DocxConfig():
     # Default set of relationshipships - the minimum components of a document
     relationships = relationshiplist()
 
@@ -41,22 +83,60 @@ if __name__ == '__main__':
     # This xpath location is where most interesting content lives
     body = document.xpath('/w:document/w:body', namespaces=nsprefixes)[0]
 
+    def add_image(self, image_file, image_caption):
+        self.relationships, picpara = picture(relationships, 
+                                                   image_file,
+                                                   image_caption)
+        self.body.append(picpara)        
+    # end add_image
+
+    def save(self, out_file):
+        # Create our properties, contenttypes, and other support files
+    
+        coreprops = coreproperties(title=title, subject=subject, creator=creator,
+                               keywords=keywords)
+        appprops = appproperties()
+        content_types = contenttypes()
+        web_settings = websettings()
+        word_relationships = wordrelationships(self.relationships)
+
+        #print 'About to save'
+        # Save our document
+        savedocx(self.document, coreprops, appprops, content_types, web_settings,
+                 word_relationships, out_file)
+    # end save
+
+    def write_heading(self, heading_text, heading_level):
+        self.body.append(heading(heading_text, 
+                                 heading_level))
+
+    def write_paragraph(self, para, row_id):
+        try:
+            self.body.append(paragraph(para))
+        except:
+            self.body.append(paragraph(
+                'Failed to write paragraph with id %s' % row_id))
+    # end write_paragraph
+# DocxConfig
+    
+if __name__ == '__main__':
+
+    s = MySettings()
+    s.read_json()
+    print s.l_delim
+    print s.r_delim
+
+    out_docx = DocxConfig()
+
     # input_file = open('export.csv')
     # input_dat = input_file.read()
     # input_file.close()
 
-    def write_paragraph(para, row_id):
-        try:
-            body.append(paragraph(para))
-        except:
-            body.append(paragraph(
-                'Failed to write paragraph with id %s' % row_id))
-
     def handle_token(token):
         # Add an image
-        relationships, picpara = picture(relationships, 'image1.png',
-                                         'Captions not implemented - TBD')
-        body.append(picpara)        
+        out_docx.add_image( 'image1.png',
+                            'Captions not implemented - TBD')
+    # end handle_token
 
     def output_body_to_docx(body, row_id):
         non_matches = re.split('{[^}]*}',body)
@@ -65,29 +145,33 @@ if __name__ == '__main__':
             raise Exception( "Need an error to throw...  todo")
 
         for i in range(0,len(matches)):
-            write_paragraph(non_matches[i],row_id)
+            out_docx.write_paragraph(non_matches[i],row_id)
             handle_token(matches[i])
-        write_paragraph(non_matches[-1],row_id)
-
+        out_docx.write_paragraph(non_matches[-1],row_id)
+    # end output_body_to_docx
 
     def output_row_to_docx(row):
 
         if len(row[HEADING_LEVEL_IND]):
-            body.append(heading(' '.join((row[HEADING_NUM_IND],row[HEADING_TEXT_IND])), 
-                                int(row[HEADING_LEVEL_IND])))
+            h_text = ' '.join((row[HEADING_NUM_IND],
+                               row[HEADING_TEXT_IND]))
+            out_docx.write_heading(h_text, 
+                                   int(row[HEADING_LEVEL_IND]))
         else:
             output_body_to_docx(row[BODY_TEXT_IND],row[ID_IND])
-    
+    # end output_row_to_docx
 
-    with open(INPUT_FILE,'rb') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-        already_skipped_header = False
-        for row in reader:
-            if already_skipped_header:
-                output_row_to_docx(row)
-            else:
-                already_skipped_header = True
-
+    try:
+        with open(INPUT_FILE,'rb') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+            already_skipped_header = False
+            for row in reader:
+                if already_skipped_header:
+                    output_row_to_docx(row)
+                else:
+                    already_skipped_header = True
+    except IOError:
+        sys.exit('Failed to open input file: %s\nExiting...' % INPUT_FILE)
 
     # for this_line in input_lines[1:7]:  # Skip heading line
     #     #cols = re.findall("[^,]*",this_line)
@@ -170,18 +254,7 @@ if __name__ == '__main__':
     # body.append(heading('Ideas? Questions? Want to contribute?', 2))
     # body.append(paragraph('Email <python.docx@librelist.com>'))
 
-    # Create our properties, contenttypes, and other support files
 
-    coreprops = coreproperties(title=title, subject=subject, creator=creator,
-                               keywords=keywords)
-    appprops = appproperties()
-    contenttypes = contenttypes()
-    websettings = websettings()
-    wordrelationships = wordrelationships(relationships)
-
-    print 'About to save'
-    # Save our document
-    savedocx(document, coreprops, appprops, contenttypes, websettings,
-             wordrelationships, OUTPUT_FILE)
-    print 'Done saving'
+    out_docx.save(OUTPUT_FILE)
+    print 'Done :-)'
 
